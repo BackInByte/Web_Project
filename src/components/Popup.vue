cdb9ebccc3ab50786597c3e7d8c57a422cad508e
Axel <axel.dran@hotmail.fr>
2019-10-27 18:33:40 +0100

    Ajout de conditions de validation pour l'ajout d'events

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -16,24 +16,27 @@
         </v-card-title>
 
         <v-card-text>
-          Please fill the following fields :
-          <v-text-field placeholder="enter title" label="Title" v-model="title" @keyup.enter="submit"></v-text-field>
-          <v-text-field placeholder="enter description" label="description" v-model="description" @keyup.enter="submit"></v-text-field>
+          <v-form ref="Popup">
 
-          <v-menu>
+            Please fill the following fields :
+            <v-text-field placeholder="enter title" label="Title" v-model="title" :rules="inputRules" @keyup.enter="submit"></v-text-field>
+            <v-text-field placeholder="enter description" label="description" v-model="description" :rules="inputRules" @keyup.enter="submit"></v-text-field>
 
-            <template v-slot:activator="{ on }">
-              <v-text-field :value="event_date" label="Event date" v-on="on"></v-text-field>
-            </template>
+            <v-menu>
 
-            <v-date-picker v-model="event_date" color="light blue"></v-date-picker>
+              <template v-slot:activator="{ on }">
+                <v-text-field :value="event_date" label="Event date" :rules="inputRules" v-on="on"></v-text-field>
+              </template>
 
-          </v-menu>
+              <v-date-picker v-model="event_date" color="light blue"></v-date-picker>
 
-          <v-btn class="success" @click="submit">
-            Save
-          </v-btn>
+            </v-menu>
 
+            <v-btn class="success" @click="submit">
+              Save
+            </v-btn>
+
+          </v-form>
         </v-card-text>
 
       </v-card>
@@ -49,25 +52,30 @@ export default {
       title: '',
       description: '',
       event_date: '',
-      dialog: false
+      dialog: false,
+      inputRules: [
+        v => v.length > 0 || 'Champ requis'
+      ]
     }
   },
   methods: {
     submit () {
+      if (this.$refs.Popup.validate()) {
       // console.log(this.title, this.description, this.event_date)
       // à terme utiliser les données récupérées pour les stocker avec d'autres évènements puis les afficher sous la forme d'un calendrier
-      this.$emit('newEvent1', {
-        title: this.title,
-        description: this.description,
-        event_date: this.event_date
-      })
-      //, this.description, this.event_date)
-      // this.$emit('newEvent2', this.description)
-      // this.$emit('newEvent3', this.event_date)
-      this.title = ''
-      this.description = ''
-      this.event_date = ''
-      this.dialog = false
+        this.$emit('newEvent1', {
+          title: this.title,
+          description: this.description,
+          event_date: this.event_date
+        })
+        //, this.description, this.event_date)
+        // this.$emit('newEvent2', this.description)
+        // this.$emit('newEvent3', this.event_date)
+        this.title = ''
+        this.description = ''
+        this.event_date = ''
+        this.dialog = false
+      }
     }
   }
 }
