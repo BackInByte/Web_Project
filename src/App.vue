0bfc904e7da78fc3aa533732c16a986cd076bf82
Seber <seb.ber.net@hotmail.fr>
2019-10-30 18:30:12 +0100

    Correctifs pour glitch 3

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,13 @@
 <template>
   <v-app>
-    <!--<v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Life Manager</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>-->
-
     <v-content>
       <Toolbar></Toolbar>
       <router-view></router-view>
-      <!--<Popup @newEvent1="updateEvent1"/>
-      <Calendar :event='evt'/>-->
-
-      <!--<p>Bonjour</p>-->
     </v-content>
-    <!--<div id="app">
-        <img src="./assets/logo.png">
-        <div>
-            <router-link :to="{ name: 'Dashboard'}">Dashboard</router-link>
-            <router-link :to="{ name: 'Login'}">Login</router-link>
-            <a href="/" v-on:click="logout">Logout</a>
-        </div>
-    </div>-->
   </v-app>
 </template>
 
 <script>
-// import Popup from './components/Popup'
-// import Calendar from './components/Calendar'
 import router from './router'
 import axios from 'axios'
 import Toolbar from './components/Toolbar'
@@ -44,8 +15,6 @@ import Toolbar from './components/Toolbar'
 export default {
   name: 'App',
   components: {
-    /* Popup,
-    Calendar */
     Toolbar
   },
   data: function () {
@@ -69,17 +38,6 @@ export default {
           router.push('/')
         })
     }
-
-    /*
-    updateEvent2 (variable) {
-      this.childData2 = variable
-      console.log(this.childData2)
-    },
-    updateEvent3 (variable) {
-      this.childData3 = variable
-      console.log(this.childData3)
-    }
-    */
   }
 }
 </script>
